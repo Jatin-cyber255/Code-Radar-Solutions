c76161be68e7fc0aebe15e7287756077b04eb5a9
Jatin-cyber255 <jatinsahil45@gmail.com>
2025-01-22 10:17:05 +0530

    Solution to Logical NOT Operation

--- Logical NOT Operation@@67882a731b4392d8fc1577de/code.c
+++ Logical NOT Operation@@67882a731b4392d8fc1577de/code.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 
-char* welcome() {
-    return "Welcome to Code Radar!";
-}
 
 int main() {
-    printf("%s", welcome());
-    return 0;
+    int a;
+    scanf("%d",&a);
+    if(a<0){
+        printf("False");
+    }else{
+        printf("True");
+    }
 }
