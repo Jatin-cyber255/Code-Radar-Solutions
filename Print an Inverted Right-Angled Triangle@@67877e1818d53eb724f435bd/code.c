6eb76f609e3ca0d860b23470a41c5db84b026393
Jatin-cyber255 <jatinsahil45@gmail.com>
2025-01-23 09:35:14 +0530

    Solution to Print an Inverted Right-Angled Triangle

--- Print an Inverted Right-Angled Triangle@@67877e1818d53eb724f435bd/code.c
+++ Print an Inverted Right-Angled Triangle@@67877e1818d53eb724f435bd/code.c
@@ -4,7 +4,7 @@ int main() {
     int a;
     scanf("%d",&a);
     for(int i=a;i<5;i--){
-        for(int j=i+1;j<a;j--){
+        for(int j=i+1;j<5;j--){
             printf("*",end=" ");
         }
     }
