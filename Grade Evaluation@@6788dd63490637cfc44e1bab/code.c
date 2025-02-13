53d218a5ac25f15b42c0c602c4b4541932a2a8a0
Jatin-cyber255 <jatinsahil45@gmail.com>
2025-02-13 10:31:10 +0530

    Solution to Grade Evaluation

--- Grade Evaluation@@6788dd63490637cfc44e1bab/code.c
+++ Grade Evaluation@@6788dd63490637cfc44e1bab/code.c
@@ -10,7 +10,7 @@ int main (){
     printf("Average");
     else if(a=='D')
     printf("Below Average");
-    else if(a==''F)
+    else if(a=='F')
     printf("Fail");
     else
     printf("Invalid grade");
